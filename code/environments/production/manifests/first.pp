68dd6701354bfe2fef86eed1bf03e4934b25bee1
Indrajeet Kumar <indrajeet@tika-btm-lp.tika-work.com>
2018-12-09 08:30:02 -0800

    Fixing password error

--- code/environments/production/manifests/first.pp
+++ code/environments/production/manifests/first.pp
@@ -2,8 +2,8 @@ user {  'deploy':
 
   ensure  => present,
   uid     => 5001,
-  home        =>'/home/deploy'
-  password    =>'$1$XZlRhcGW$UWV9Xg0m50ofHdd/3766S1'
-  managehome  =>true
+  home        => '/home/deploy'
+  password    => '$1$XZlRhcGW$UWV9Xg0m50ofHdd/3766S1'
+  managehome  => true
 
 }

bf30b4a31fbf6b571e02f5304230a79fcfd50c1e
Indrajeet Kumar <indrajeet@tika-btm-lp.tika-work.com>
2018-12-11 04:50:38 -0800

    dgfg

---
+++ code/environments/production/modules/java/manifests/params.pp
@@ -0,0 +1,13 @@
+class java::params {
+    
+$user = 'tomcat'
+$group = 'tomcat'
+$config_path = '/etc/tomcat/tomcat.conf'
+$packages = [ 'epel-release', 'java' ]
+$service_state = 'running'
+$service_name = 'tomcat'
+$service_enable = 'true'
+
+
+
+}

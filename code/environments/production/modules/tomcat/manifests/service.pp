fe1a678e33ed2c6bd8cd0d3ca8febaa344f0a654
Indrajeet Kumar <indrajeet@tika-btm-lp.tika-work.com>
2018-12-11 02:32:43 -0800

    sdfd

--- code/environments/production/modules/tomcat/manifests/service.pp
+++ code/environments/production/modules/tomcat/manifests/service.pp
@@ -1,8 +1,9 @@
 class tomcat::service inherits tomcat {
 
-    service { $::tocat::service_name:
-    ensure => $::tomcat::service_state,
-    #enable => $::tomcat::service_enable,
+    service{ $::tocat::service_name:
+
+      ensure => $::tomcat::service_state,
+      enable => true,
 
     }
 }

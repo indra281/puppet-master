27410b0591e43bd73b8f0510524ce1f7e16b1b80
Indrajeet Kumar <indrajeet@tika-btm-lp.tika-work.com>
2018-12-11 01:13:40 -0800

    adding param class

--- code/environments/production/manifests/node.pp
+++ code/environments/production/manifests/node.pp
@@ -1,22 +1,6 @@
-node 'app-1' {
-
-    include java::install
-}
-
 node nginx-prod2 {
 
-    notify { "Nginx Prod 2 Block ":}
-
-    include tomcat
-    include prefs
-
-    
-}
-
-node nginx-prod1 { 
-
-notify { "this block id for nginx prod1 block":}
-include java
 include tomcat
+
 
 }
